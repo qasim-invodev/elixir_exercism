1f07f3dbf3c7047feea0afca8fc78d00cfd22bfa
qasim-invodev <qasim.mehmood@invozone.com>
2022-06-27 18:04:29 +0500

    Freelancer and Secrets Done

---
+++ Basic_Types/freelancer_rates.exs
@@ -0,0 +1,20 @@
+#Exercise # 2: Freelancer Rates - Integers and Float Numbers
+
+defmodule FreelancerRates do
+  def daily_rate(hourly_rate) do
+    hourly_rate * 8.0
+  end
+
+  def apply_discount(before_discount, discount) do
+    discount = discount/100
+    before_discount - (before_discount * discount)
+  end
+
+  def monthly_rate(hourly_rate, discount) do
+    trunc(Float.ceil(apply_discount(daily_rate(hourly_rate) * 22, discount)))
+  end
+
+  def days_in_budget(budget, hourly_rate, discount) do
+    Float.floor(budget/apply_discount(daily_rate(hourly_rate), discount),1)
+  end
+end
---
+++ Basic_Types/make_lasagna.exs
@@ -0,0 +1,23 @@
+#First Exercise: Make Lasagna - Basic Operations
+
+defmodule Lasagna do
+  def expected_minutes_in_oven do
+    40
+  end
+
+  def remaining_minutes_in_oven(remain) do
+    expected_minutes_in_oven() - remain
+  end
+
+  def preparation_time_in_minutes(layers) do
+    layers * 2
+  end
+
+  def total_time_in_minutes(layers, minutes) do
+    preparation_time_in_minutes(layers) + minutes
+  end
+
+  def alarm() do
+    "Ding!"
+  end
+end

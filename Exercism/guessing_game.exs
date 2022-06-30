92a251e7fed4e8368dd4240bee18db2fe36d21df
qasim-invodev <qasim.mehmood@invozone.com>
2022-06-30 12:38:59 +0500

    updated_changes

--- Exercism/guessing_game.exs
+++ Exercism/guessing_game.exs
@@ -5,19 +5,10 @@
 # You need to devise a function to provide different responses depending on how the guess relates to the secret number.
 
 defmodule GuessingGame do
-  def compare(secret_number, guess) when secret_number == guess do
-    "Correct"
-  end
-  def compare(secret_number, guess) when secret_number == guess+1 or secret_number == guess-1 do
-    "So close"
-  end
-  def compare(secret_number, guess) when secret_number > guess+1 do
-    "Too low"
-  end
-  def compare(secret_number, guess) when secret_number < guess-1 do
-    "Too high"
-  end
-  def compare(secret_number, guess \\ :no_guess) do
-    "Make a guess"
-  end
+  def compare(secret, guess \\ :no_guess)
+  def compare(_secret, :no_guess), do: "Make a guess"
+  def compare(secret, guess) when secret == guess, do: "Correct"
+  def compare(secret, guess) when (secret - guess) in [-1, 1], do: "So close"
+  def compare(secret, guess) when secret < guess, do: "Too high"
+  def compare(secret, guess) when secret > guess, do: "Too low"
 end
